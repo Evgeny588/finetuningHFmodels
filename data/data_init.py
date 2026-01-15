d88f95d7c6df720082edacb0527a2698f3ead5b3
Evgeny <evgeborisov@gmail.com>
2026-01-15 13:41:57 +0300

    Interactiva train cycle

--- data/data_init.py
+++ data/data_init.py
@@ -1,14 +1,32 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, BertForSequenceClassification
 
-data_checkpoint = 'mteb/RuToxicOKMLCUPClassification'
-model_checkpoint = 'DeepPavlov/rubert-base-cased'
+data_checkpoint = 'glue', 'sst2'
+model_checkpoint = 'bert-base-uncased'
 
-dataset = load_dataset(data_checkpoint)
-
-train_data = dataset['train']
-test_data = dataset['test']
-
+dataset = load_dataset(*data_checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-model = BertForSequenceClassification.from_pretrained(model_checkpoint)
-print(model)
+
+
+def tokenize_func(batch):
+    global library
+    return tokenizer(
+        batch['sentence'],
+        truncation = True,
+        padding = False,
+    )
+
+def get_tokenized_data():
+    train = dataset['train']
+    val = dataset['validation']
+
+    tokenized_train = train.map(tokenize_func, remove_columns = ['sentence'], load_from_cache_file = False) 
+    tokenized_val = val.map(tokenize_func, remove_columns = ['sentence'], load_from_cache_file = False)
+    return tokenized_train, tokenized_val
+
+def main():
+    train_data, val_data = get_tokenized_data()
+    print("Sample from train_data:", train_data[0])
+
+if __name__ == '__main__':
+    main()
--- hf/train_script_hf.py
+++ hf/train_script_hf.py
@@ -1,20 +1,68 @@
 import logging
 import sys
+import warnings
 
 from pathlib import Path
+from transformers import (
+    BertForSequenceClassification,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+    DataCollatorWithPadding
+)
 
 root_path = Path(__file__).resolve().parent.parent
 filename = Path(__file__).stem
 sys.path.append(str(root_path))
+warnings.filterwarnings('ignore')
 
+from data.data_init import (
+    model_checkpoint,
+    get_tokenized_data,
+)
 from set_logging import setup_logging
-setup_logging(console_level = logging.WARNING, file_mode = 'w')
 
+# Logging setups
+setup_logging(console_level = logging.WARNING, file_mode = 'w')
 logging = logging.getLogger(filename)
 
-logging.debug('debug message')
-logging.info('info message')
-logging.warning('warning message')
-logging.error('error message')
-logging.critical('critical message')
 
+# Main code
+def main():
+    # Used library
+    library = 'pt'
+
+    # Model initialize
+    model = BertForSequenceClassification.from_pretrained(model_checkpoint)
+    logging.debug(f'Model {model_checkpoint} was initialized.')
+
+    # Other initializations
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    collator = DataCollatorWithPadding(tokenizer)
+
+    # Data initialize
+    train_data, val_data = get_tokenized_data()
+    logging.debug('Train and validation data initialized.')
+
+    # Arguments for train
+    train_args = TrainingArguments(
+        str(Path(root_path) / 'checkpoints'),
+        eval_strategy = 'steps',
+        eval_steps = 10,
+        logging_dir = str(Path(root_path) / 'logs')
+    )
+
+    # Trainer for model
+    trainer = Trainer(
+        model = model,
+        args = train_args,
+        data_collator = collator,
+        train_dataset = train_data,
+        eval_dataset = val_data,
+        tokenizer = tokenizer
+    )
+
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
